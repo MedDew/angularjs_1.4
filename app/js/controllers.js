542652cb2eb61141b89b08706085a13666c52c42
Med <mehdi.gacem@versusmind.eu>
2018-11-27 16:47:35 +0100

    us e DI to inject http service and get the response from the phones.json file + limit the reponse return to 5 results

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,26 +3,29 @@
 /* Controllers */
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) 
+phonecatApp.controller('PhoneListCtrl', ["$scope", "$http", function ($scope, $http)
 {
-  $scope.phones = 
-  [
-    {
-     'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.',
-     'age' : 1
-    },
-    {
-        'name': 'Motorola XOOM™ with Wi-Fi',
-        'snippet': 'The Next, Next Generation tablet.',
-        'age' : 2
-    },
-    {
-        'name': 'MOTOROLA XOOM™',
-        'snippet': 'The Next, Next Generation tablet.',
-        'age' : 3
-    }
-  ];
+  $http.get("phones/phones.json").success(function(data){
+    $scope.phones = data.splice(0,5);  
+  });
+//   $scope.phones = 
+//   [
+//     {
+//      'name': 'Nexus S',
+//      'snippet': 'Fast just got faster with Nexus S.',
+//      'age' : 1
+//     },
+//     {
+//         'name': 'Motorola XOOM™ with Wi-Fi',
+//         'snippet': 'The Next, Next Generation tablet.',
+//         'age' : 2
+//     },
+//     {
+//         'name': 'MOTOROLA XOOM™',
+//         'snippet': 'The Next, Next Generation tablet.',
+//         'age' : 3
+//     }
+//   ];
   $scope.test = function(){
     
         let names =$scope.phones.map(function(elm) {
@@ -41,5 +44,5 @@ phonecatApp.controller('PhoneListCtrl', function ($scope)
   $scope.name = "Buddy";
 
   //Default orderProp value
-//   $scope.orderProp = "age";
-});
+  $scope.orderProp = "age";
+}]);
