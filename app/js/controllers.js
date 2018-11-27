231ea89ea364769d673cbf4f1ad98b000d36c428
Med <mehdi.gacem@versusmind.eu>
2018-11-27 15:31:13 +0100

    add a age property to phones object + create a function test to check the map function + create a scope property orderProp to set a default valu for the filter orderBy

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,17 +9,37 @@ phonecatApp.controller('PhoneListCtrl', function ($scope)
   [
     {
      'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age' : 1
     },
     {
-     'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'
+        'name': 'Motorola XOOM™ with Wi-Fi',
+        'snippet': 'The Next, Next Generation tablet.',
+        'age' : 2
     },
     {
-     'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'
+        'name': 'MOTOROLA XOOM™',
+        'snippet': 'The Next, Next Generation tablet.',
+        'age' : 3
     }
   ];
+  $scope.test = function(){
+    
+        let names =$scope.phones.map(function(elm) {
+        //   for(var e in elm.name)
+        //     console.log(e+" => "+elm.name[e]);
+
+        //   console.log(elm.name.getText());
+          console.log(elm.name);
+
+          return elm.name;
+        });
+        console.log(names);
+        return names;
+  };
 
   $scope.name = "Buddy";
+
+  //Default orderProp value
+//   $scope.orderProp = "age";
 });
