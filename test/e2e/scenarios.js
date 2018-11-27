037d67006dd396520d22fba316cd509fa49a2e5d
Med <mehdi.gacem@versusmind.eu>
2018-11-27 15:34:23 +0100

    create shitty not working specification to check the filtered tablet value over the phones and check the sorting of the phone list as well

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -23,41 +23,80 @@ describe('PhoneListCtrl', function()
     console.log("YAWP");
     expect(scope.phones.length).toBe(3);
   });
-
+  
 });
 */
 
 describe('PhoneCat App', function() {
-
+  
   describe('Phone list view', function() {
-
+    
     beforeEach(function() {
       browser.get('app/index.html');
     });
-
-
-    it('should filter the phone list as a user types into the search box', function() {
-
+    
+    
+    /*it('should filter the phone list as a user types into the search box', function() {
+      
       var phoneList = element.all(by.repeater('phone in phones'));
       var query = element(by.model('query'));
-
+      
       expect(phoneList.count()).toBe(3);
-
+      
       query.sendKeys('nexus');
       expect(phoneList.count()).toBe(1);
-
+      
       query.clear();
       query.sendKeys('motorola');
       expect(phoneList.count()).toBe(2);
     });
-
+    
     it('should display the current filter value in the title bar', function() {
       var query = element(by.model('query'));
       query.clear();
       expect(browser.getTitle()).toMatch(/Google Phone Gallery :\s*$/);
-
+      
       query.sendKeys('nexus');
       expect(browser.getTitle()).toMatch(/Google Phone Gallery : nexus$/);
+    });*/
+    
+    it('should be possible to control phone order via the drop down select box', function() {
+      
+      var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
+      var query = element(by.model('query'));
+      
+      function getNames() {
+        
+        return phoneNameColumn.map(function(elm) {
+          console.log("YAWP");
+          // console.log(elm.getText());
+          return elm.getText();
+        });
+      }
+      
+      // browser.manage().logs().get("browser").then(function(browserLogs){
+      //   browserLogs.forEach(function(log) {
+      //     if(log.level > 900)
+      //     {
+      //       console.log("BROWSER LOG");
+      //     }
+      //   });
+      // });
+
+      query.sendKeys('tablet'); //let's narrow the dataset to make the test assertions shorter
+      
+      //expect(phoneNameColumn.count()).toBe(3);
+      expect(getNames()).toEqual([
+        "Motorola XOOM\u2122 with Wi-Fi",
+        "MOTOROLA XOOM\u2122"
+      ]);
+    
+      element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+    
+      expect(getNames()).toEqual([
+        "MOTOROLA XOOM\u2122",
+        "Motorola XOOM\u2122 with Wi-Fi"
+      ]);
     });
 
   });
