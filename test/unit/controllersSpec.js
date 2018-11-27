fddfd4df3b79d761cb0a03d769349b70e85c7a7d
Med <mehdi.gacem@versusmind.eu>
2018-11-27 16:51:17 +0100

    add 2 assertions to check the result from httpBackend mock version of http service and check that the orderProp default value is age

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -27,6 +27,8 @@ describe('controllers', function() {
 
 });
 */
+
+/*
 describe("Phonecat Controllers", function(){
   
   describe("PhoneListCtrl", function(){
@@ -40,7 +42,7 @@ describe("Phonecat Controllers", function(){
     }));
 
     it('should create "phones" model with 3 phones', function() {
-      expect(scope.phones.length).toBe(3);
+      expect(scope.phones.length).toBe(20);
     });
 
 
@@ -50,4 +52,38 @@ describe("Phonecat Controllers", function(){
 
   });
 
+});
+*/
+
+describe('PhoneCat controllers', function() {
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl, $httpBackend;
+  
+    // Load our app module definition before each test.
+    beforeEach(module('phonecatApp'));
+  
+    // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+    // This allows us to inject a service but then attach it to a variable
+    // with the same name as the service in order to avoid a name conflict.
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/phones.json').
+          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+  
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneListCtrl', {$scope: scope});
+    }));
+
+    it('should create "phones" model with 2 phones fetched from xhr', function() {
+      expect(scope.phones).toBeUndefined();
+      $httpBackend.flush();
+       expect(scope.phones).toEqual([{name: 'Nexus S'},
+                                   {name: 'Motorola DROID'}]);
+    });
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+
+  });
 });
