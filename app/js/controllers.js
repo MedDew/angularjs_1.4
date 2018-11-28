c67f38578023eb3686a5104be41e0349f925d220
Med <mehdi.gacem@versusmind.eu>
2018-11-28 11:29:28 +0100

    remove the code to list phones + add the use of ng-view + declare app.js to use the needed module + declare angular-route.js to handle the routing

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', ["$scope", "$http", function ($scope, $http)
+phonecatControllers.controller('PhoneListCtrl', ["$scope", "$http", function ($scope, $http)
 {
   $http.get("phones/phones.json").success(function(data){
     $scope.phones = data.splice(0,5);  
@@ -46,3 +46,11 @@ phonecatApp.controller('PhoneListCtrl', ["$scope", "$http", function ($scope, $h
   //Default orderProp value
   $scope.orderProp = "age";
 }]);
+
+phonecatControllers.controller("PhoneDetailCtrl", ["$scope", "$routeParams", 
+    function($scope, $routeParams)
+    {
+        $scope.phoneId = $routeParams.phoneId;
+    }
+
+]);
